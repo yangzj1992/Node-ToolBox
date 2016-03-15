1d5c767d0955004a4ed270e9dd835d34fb2a1c4b
yangzj1992 <yangzj1992@qq.com>
2016-03-15 17:31:10 +0800

    add

---
+++ async_spider.js
@@ -0,0 +1,46 @@
+var async = require("async");
+var request = require('superagent');
+var cheerio = require('cheerio');
+
+var url = 'qcyoung.com';
+async.waterfall([
+  function(callback) {
+    request
+      .get(url + '/archives')
+      .end(function(err, res) {
+        if(err){
+          console.log(err.message);
+        }
+        var $ = cheerio.load(res.text);
+        var urls = [];
+        $('#services .title a').each(function(id, elem) {
+          urls.push(url + $(elem).attr('href'));
+        });
+        callback(null, urls);
+      });
+  },
+  function(urls, callback) {
+    async.mapLimit(urls, 5, function(urlId, callback) { //并发数是5
+      var d = new Date();
+      console.log(d.getSeconds() + 's ' + d.getMilliseconds() + 'ms ' + '正在抓取：' + urlId);
+      urlId = encodeURI(urlId);
+      request
+        .get(urlId)
+        .on('error',function(err){
+          if(err){
+            console.log(err.message);
+          }
+        })
+        .end(function(err, res) {
+          var $ = cheerio.load(res.text);
+          callback(null, $('.summary .post-words').text());
+        });
+    }, function(err, res) {
+      console.log('文章标题:');
+      console.log(res);
+      callback();
+    });
+  }], function(err, res) {
+    console.log(res);
+  }
+);
